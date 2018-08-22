303360be331446b4c12611b324570c392a3ec0a8
XuJinL1874 <164833192@qq.com>
2018-08-22 13:22:26 +0800

    JUnit

--- WEB08_JDBC/src/cn/itmastc/test/QueryAll.java
+++ WEB08_JDBC/src/cn/itmastc/test/QueryAll.java
@@ -15,7 +15,14 @@ import org.junit.Test;
  * @Modified By:
  */
 public class QueryAll {
-
+    /**
+     * 执行SQL语句
+     * int executeUpdate(String sql) 执行insert update delete语句(DML语句)
+     * ResultSet executeQuery(String sql) 执行select语句(DQL语句)
+     * boolean execute(String sql) 执行select返回true,执行其他语句返回false
+     *      如果返回true,需要使用getResultSet()获得查询结果
+     *      如果返回false,需要使用getUpdateCount()获得影响行数
+     */
 
     @Test
     public void testQueryAll(){
@@ -28,17 +35,18 @@ public class QueryAll {
             //2.获取连接
             String url ="jdbc:mysql://127.0.0.1:3306/web08?useUnicode=true&characterEncoding=utf8";
             String username="root";
-            String password="root";
+            String password="123456";
             conn = DriverManager.getConnection(url,username,password);
             //3.获取执行sql语句对象
             stmt = conn.createStatement();
             //4.编写sql语句
-            String sql = "select * from tbl_user";
+            String sql = "select * from product";
             //5.执行sql语句
             rs = stmt.executeQuery(sql);
             //6.处理结果集
             while(rs.next()){
-                System.out.println("用户名："+rs.getString(2)+" 密码："+rs.getString("upassword"));
+                System.out.println("用户名："+rs.getString(2)+" 价格："+rs.getString(
+                        "price"));
             }
         } catch (Exception e) {
             e.printStackTrace();
