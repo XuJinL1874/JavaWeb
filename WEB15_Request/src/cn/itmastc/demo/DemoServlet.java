3879066c28ce9d120c80c6334112dd9f6976b5dc
XuJinL1874 <164833192@qq.com>
2018-08-28 00:24:17 +0800

    request

---
+++ WEB15_Request/src/cn/itmastc/demo/DemoServlet.java
@@ -0,0 +1,27 @@
+package cn.itmastc.demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author: X_JinL
+ * @Description:
+ * @Date: Created in 21:48 2018/8/27
+ * @Modified By:
+ */
+@WebServlet(name = "DemoServlet", urlPatterns = {"/demoServlet"})
+public class DemoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().write("Welcome To There...");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
