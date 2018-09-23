ed503a3b9c3611463ee2f72ddfcf0ce267c984ce
XuJinL1874 <164833192@qq.com>
2018-09-23 21:34:35 +0800

    redis

---
+++ WEB26_Redis/src/cn/mastc/jedis/JedisTest.java
@@ -0,0 +1,31 @@
+package cn.mastc.jedis;
+
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+/**
+ * @Author: X_JinL
+ * @Description:
+ * @Date: Created in 19:58 2018/9/23
+ * @Modified By:
+ */
+public class JedisTest {
+
+    // 通过java程序访问redis数据库
+
+    @Test
+    // 获得单一的jedis对象操作数据库
+    public void test1() {
+        // 1.获得连接对象
+        Jedis jedis = new Jedis("192.168.1.200", 6379);
+
+        // 2.获得连接数据
+        String username = jedis.get("username");
+        System.out.println(username);
+
+        // 3.存储
+        jedis.set("addr", "shanghai");
+        System.out.println(jedis.get("addr"));
+    }
+
+}
