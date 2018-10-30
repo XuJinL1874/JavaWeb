a7dbb72180ae922795049239b9bbba2127f4ff97
XuJinL1874 <164833192@qq.com>
2018-10-30 17:23:56 +0800

    spring_01

---
+++ WEB28_Spring/src/cn/mastc/demo/Demo.java
@@ -0,0 +1,27 @@
+package cn.mastc.demo;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author: X_JinL
+ * @Description:
+ * @Date: Created in 16:38 2018/10/30
+ * @Modified By:
+ */
+public class Demo {
+
+    @Test
+    public void fun1(){
+
+        // 1.创建容器对象
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // 2.向容器"要"user对象
+        Object user = applicationContext.getBean("user");
+        // 3.打印user对象
+        System.out.println(user);
+
+    }
+
+}
