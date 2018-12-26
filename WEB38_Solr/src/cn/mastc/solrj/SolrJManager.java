4f51f668de307dffcf12c28de0bbb3cdfe3926ed
XuJinL1874 <164833192@qq.com>
2018-12-26 15:48:04 +0800

    solr

---
+++ WEB38_Solr/src/cn/mastc/solrj/SolrJManager.java
@@ -0,0 +1,27 @@
+package cn.mastc.solrj;
+
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.common.SolrInputDocument;
+
+import java.io.IOException;
+
+/**
+ * @author X_JinL
+ * @version 1.0.0
+ * @ClassName SolrJManager.java
+ * @Description solrJ管理
+ *               添加
+ *               删除
+ *               修改
+ *               查询
+ * @createTime 2018/12/25 20:48
+ */
+public class SolrJManager {
+
+    //指定solr服务器的地址
+    private final static String SOLR_URL = "http://localhost:8080/solr/";
+
+
+
+}
